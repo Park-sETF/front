42204ed01b811af054cf7d25e5d640c39cd9555c
cie10 <leesky0075@dgu.ac.kr>
2024-11-16 18:15:50 +0900

    feat: create Init page (#1)

--- src/routes/init/page.jsx
+++ src/routes/init/page.jsx
@@ -1,5 +1,31 @@
 export default function Init() {
   return (
-    <div>Init</div>
-  )
+    <div
+      className="min-vh-100 d-flex justify-content-center align-items-center bg-white"
+      style={{width: '100%', maxWidth: '430px', margin: '0 auto', textAlign: 'center',height:'100vh',
+          display: 'flex', justifyContent:'center', flexDirection: 'column'
+       }}
+    >
+      <h1 className="display-5 fw-bold text-center" style={{ fontSize: '42px', marginBottom: '20px'
+       }}>
+        What's your ETF?
+      </h1>
+
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ width: '100%', maxWidth: '280px', margin: '0 auto'}}
+      >
+        <img
+          src="/images/logo.png"
+          alt="ETF Character"
+          style={{
+            width: '100%',
+            height: 'auto',
+            objectFit: 'contain',
+            display: 'block',
+          }}
+        />
+      </div>
+    </div>
+  );
 }
