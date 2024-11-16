aeb0b5b41fcdf37d0eb57e9e65b70a54f087a723
cie10 <leesky0075@dgu.ac.kr>
2024-11-16 21:25:23 +0900

    feat: edit Home/ page (#2)

--- src/routes/user/page.jsx
+++ src/routes/user/page.jsx
@@ -1,11 +1,14 @@
 import UserInfo from "~/components/Home/UserInfo"
 import Tab from "~/components/Home/Tab"
+import Footer from "~/components/MyFooter"
 
 export default function User() {
   return (
     <div>
       <UserInfo></UserInfo>
       <Tab></Tab>
+      <Footer></Footer>
+
     </div>
   )
 }
