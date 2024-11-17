a4ef84b55818b1b30a6e2b87d38acc273dd9180a
cie10 <leesky0075@dgu.ac.kr>
2024-11-17 16:45:02 +0900

    chore: edit homepage (#2)

--- src/routes/user/page.jsx
+++ src/routes/user/page.jsx
@@ -1,12 +1,14 @@
 import UserInfo from "~/components/Home/UserInfo"
 import Tab from "~/components/Home/Tab"
 import Footer from "~/components/MyFooter"
+import BigButton from "~/components/buttons/BigButton"
 
 export default function User() {
   return (
-    <div>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
       <UserInfo></UserInfo>
       <Tab></Tab>
+      <BigButton></BigButton>
       <Footer></Footer>
 
     </div>
