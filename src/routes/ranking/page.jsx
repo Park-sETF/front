f52c8699942260d57cd7c1a46dddd1fc60eae55f
greenlemonT <ms950219@naver.com>
2024-11-22 10:55:16 +0900

    feat: ranking by user revenue (#22)

--- src/routes/ranking/page.jsx
+++ src/routes/ranking/page.jsx
@@ -1,5 +1,7 @@
-export default function ranking() {
-  return (
-    <div>ranking</div>
-  )
-}
+import RankingPage from '~/components/Ranking/RankingPage';
+
+const RankingRoute = () => {
+  return <RankingPage />;
+};
+
+export default RankingRoute;
