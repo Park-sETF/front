9b9a56c9184eed23c7ae0bc9b339ed9fb7b3fc92
DrivenByCode <jaykey1004@gmail.com>
2024-11-25 14:42:57 +0900

    fix: ranking header(#37)

--- src/routes/signup/page.jsx
+++ src/routes/signup/page.jsx
@@ -190,7 +190,6 @@ function Signup() {
                       {errors.password}
                     </Form.Control.Feedback>
                     <Button
-                      variant="success"
                       type="submit"
                       className="w-100 mt-3"
                       disabled={
