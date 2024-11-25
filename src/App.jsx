ac52d72522e345fc46d8b34353b5cea4fee4bae0
cie10 <leesky0075@dgu.ac.kr>
2024-11-25 13:13:55 +0900

    feat: Wrap application with <StockProvider> to manage stock selection state globally using Context API (#27)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import mainRouter from '~/routers/main-router';
 import { RouterProvider } from 'react-router-dom';
+import {StockProvider} from './components/context/StockProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <StockProvider>
       <RouterProvider router={mainRouter} />
+    </StockProvider>
   )
 }
 
