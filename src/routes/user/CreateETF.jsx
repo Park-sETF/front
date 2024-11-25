5c433185f4a625aa81e58370a22d61beccde6595
DrivenByCode <jaykey1004@gmail.com>
2024-11-25 20:08:05 +0900

    fix: chart component(#6)

--- src/routes/user/CreateETF.jsx
+++ src/routes/user/CreateETF.jsx
@@ -4,34 +4,11 @@ import { useState } from 'react';
 
 export default function ETFPocket() {
   const [stocks, setStocks] = useState([
-    { name: '삼성전자', percentage: 40, price: 60000, color: colorPalette[0] },
-    {
-      name: '현대자동차',
-      percentage: 35,
-      price: 230000,
-      // color: colorPalette[1],
-      // color: generatePastelColor(),
-    },
-    {
-      name: '하이닉스',
-      percentage: 12,
-      price: 230000,
-      // color: colorPalette[2],
-
-      // color: generatePastelColor(),
-    },
-    {
-      name: 'LG전자',
-      percentage: 3,
-      price: 80000,
-      // color: colorPalette[3],
-    },
-    {
-      name: '한화에어로스페이스',
-      percentage: 10,
-      price: 300000,
-      // color: colorPalette[4],
-    },
+    { name: '삼성전자', percentage: 40, price: 60000 },
+    { name: '현대자동차', percentage: 35, price: 230000 },
+    { name: '하이닉스', percentage: 12, price: 230000 },
+    { name: 'LG전자', percentage: 3, price: 80000 },
+    { name: '한화에어로스페이스', percentage: 10, price: 300000 },
   ]);
 
   const addStock = () => {
@@ -42,6 +19,7 @@ export default function ETFPocket() {
     };
     setStocks([...stocks, newStock]);
   };
+
   return (
     <div>
       <Chart stocks={stocks} addStock={addStock} setStocks={setStocks} />
