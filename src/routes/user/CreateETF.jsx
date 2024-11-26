a0d10fad7dd17537f6f1dfe46644c5f084dad439
cie10 <leesky0075@dgu.ac.kr>
2024-11-26 15:49:13 +0900

    feat: Apply context in CreateETF (#39)

--- src/routes/user/CreateETF.jsx
+++ src/routes/user/CreateETF.jsx
@@ -1,29 +1,51 @@
 import BigButton from '~/components/buttons/BigButton';
 import Chart from '~/components/MyPocket/Chart';
-import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useStockContext } from '~/components/context/StockProvider';
+
 
 export default function ETFPocket() {
-  const [stocks, setStocks] = useState([
-    { name: '삼성전자', price: 60000 },
-    { name: '현대자동차', price: 230000 },
-    { name: '하이닉스', price: 230000 },
-    { name: 'LG전자', price: 80000 },
-    { name: '한화에어로스페이스', price: 300000 },
-  ]);
+
+  // 임시로 지정함! 
+  const userId = 1; 
+
+  const navigate = useNavigate();
+
+  const {selectedStocks,setSelectedStocks} = useStockContext(); // context로 가져오기 
 
   const addStock = () => {
-    const newStock = {
-      name: `주식 ${stocks.length + 1}`,
-      percentage: 0,
-      price: 0,
-    };
-    setStocks([...stocks, newStock]);
+    navigate('/select-stock'); // 추가하기 버튼 클릭시 종목 선택하기 페이지로 이동 
   };
+
+  const addButtonClick = async () => {
+    try {
+      // 서버에 전달할 데이터 구성 (예: 선택된 종목)
+      const requestData = {
+        userId,
+        selectedStocks,
+      };
+  
+      // POST 요청
+      const response = await axios.post(
+        `http://localhost:8080/api/etf/buy/${userId}`,
+        requestData
+      );
+  
+      // 성공 메시지 확인 또는 후속 처리
+      console.log('ETF 투자 성공', response.data);
+      alert('투자 요청이 성공적으로 완료되었습니다!');
+    } catch (error) {
+      console.error('ETF 투자하기 오류', error);
+      alert('투자 요청 중 오류가 발생했습니다.');
+    }
+  };
+  
 
   return (
     <div>
-      <Chart stocks={stocks} addStock={addStock} setStocks={setStocks} />
-      <BigButton text={'투자하기'} />
+      <Chart stocks={selectedStocks} addStock={addStock} setStocks={setSelectedStocks} />
+      <BigButton text={'투자하기'} onClick={addButtonClick}/>
     </div>
   );
 }
