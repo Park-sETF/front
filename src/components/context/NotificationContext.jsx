88b0fd0b6b74c727f4ef2a927e2420b3f945a106
greenlemonT <ms950219@naver.com>
2024-12-05 08:31:43 +0900

    fix: alarm

--- src/components/context/NotificationContext.jsx
+++ src/components/context/NotificationContext.jsx
@@ -13,20 +13,16 @@ export function NotificationProvider({ children }) {
       console.log("SSE 연결이 열렸습니다.");
     };
 
-     // notice 이벤트 수신
-  eventSource.addEventListener("notice", (event) => {
-    try {
-      const newNotification = JSON.parse(event.data);
-      console.log("수신된 알림:", newNotification);
-
-      // 현재 로그인한 사용자의 알림인지 확인
-      if (newNotification.userId === parseInt(id, 10)) {
+    // notice 이벤트 수신
+    eventSource.addEventListener("notice", (event) => {
+      try {
+        const newNotification = JSON.parse(event.data);
+        console.log("수신된 알림:", newNotification);
         setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
+      } catch (error) {
+        console.error("알림 데이터 처리 중 오류 발생:", error);
       }
-    } catch (error) {
-      console.error("알림 데이터 처리 중 오류 발생:", error);
-    }
-  });
+    });
 
     eventSource.onerror = () => {
       console.error("SSE 연결 오류 발생");
